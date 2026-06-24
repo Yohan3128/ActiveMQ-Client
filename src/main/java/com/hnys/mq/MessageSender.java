70d27d2e1f9af41e9ac88647940583e5b7bac1f7
Yohan3128 <Yohansilva560@gmail.com>
2026-06-24 14:21:08 +0530

    Implement message scheduling in MessageSender and add VCS configuration

--- src/main/java/com/hnys/mq/MessageSender.java
+++ src/main/java/com/hnys/mq/MessageSender.java
@@ -1,4 +1,36 @@
 package com.hnys.mq;
 
+import jakarta.jms.*;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ScheduledMessage;
+
 public class MessageSender {
+    public static void main(String[] args) {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+//        connectionFactory.setBrokerURL("tcp://localhost:61616");
+
+        try {
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic topic = session.createTopic("news");
+
+            MessageProducer producer = session.createProducer(topic);
+
+            TextMessage message = session.createTextMessage("Hello World");
+
+            message.setStringProperty(ScheduledMessage.AMQ_SCHEDULED_CRON, "* * * * *");
+
+            producer.send(message);
+
+            producer.close();
+            session.close();
+            connection.close();
+
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
